075bbeefc550896193ac27593a77c2642858af34
Vitali Kuchynski <vitali.kuchynski@gmail.com>
2018-03-03 21:09:39 -0500

    json report

--- src/test/java/framework/TestRunner.java
+++ src/test/java/framework/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
     @RunWith(Cucumber.class)
     @CucumberOptions(
             // This creates cucumber reports
-            plugin = {"pretty", "html:target/site/cucumber-pretty"},
+            plugin = {"pretty", "html:target/site/cucumber-pretty","json:target/site/cucumber-pretty/cucumber.json"},
             //This create auto method name in camelCase
             snippets = cucumber.api.SnippetType.CAMELCASE,
 
